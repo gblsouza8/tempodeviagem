9ffd7959079ae2e691aab9078c11a7eebd81507b
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-04-03 10:16:09 -0300

    Segundo Commit

--- tempodeviagem/src/main/java/tempodeviagem/Main.java
+++ tempodeviagem/src/main/java/tempodeviagem/Main.java
@@ -2,6 +2,7 @@ package tempodeviagem;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        
     }
 }
